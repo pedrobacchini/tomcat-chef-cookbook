07f7199f07f14048193adad665df7a7124afd031
root <root@201-16-197-5.vm.example.com>
2018-07-17 18:59:30 +0000

    Primeiro Commit - First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# tomcat-chef-cookbook
+
+Livro de receitas de instalação do tomcat 8 no centos 7 para o Chef, baseado no tutorial encontrado 
+em: https://www.digitalocean.com/community/tutorials/how-to-install-apache-tomcat-8-on-centos-7 
+
+Cookbook for installing Tomcat 8 in centos 7 for Chef, based on the tutorial found 
+at: https://www.digitalocean.com/community/tutorials/how-to-install-apache-tomcat-8-on-centos-7
---
+++ recipes/default.rb
@@ -0,0 +1,49 @@
+#
+# Cookbook:: tomcat
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'java-1.8.0-openjdk-devel'
+
+group 'tomcat'
+
+user 'tomcat' do
+	manage_home false
+	shell '/bin/nologin'
+	group 'tomcat'
+	home '/opt/tomcat'
+end
+
+remote_file '/opt/apache-tomcat-8.5.9.tar.gz' do
+	source 'http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.32/bin/apache-tomcat-8.5.32.tar.gz'
+end
+
+#execute 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+execute 'extract_tar' do
+  command 'tar xvf apache-tomcat-8.5.9.tar.gz && mv apache-tomcat-8.5.32 tomcat'
+  cwd '/opt'
+  not_if { File.exists?("/opt/tomcat") }
+end
+
+execute 'chgrp -R tomcat /opt/tomcat'
+
+execute 'chmod -R g+r /opt/tomcat/conf'
+
+execute 'chmod g+x /opt/tomcat/conf'
+
+execute 'chown -R tomcat webapps/ work/ temp/ logs/' do
+	cwd '/opt/tomcat/'
+end
+
+template '/etc/systemd/system/tomcat.service' do
+	source 'tomcat.service.erb'
+end
+
+execute 'daemon-reload' do
+  command "systemctl daemon-reload"
+end
+
+service 'tomcat' do
+	action [:start, :enable]
+end
